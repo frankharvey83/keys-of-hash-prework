dd209f7a74faa83083a9307ec3df5366fbdda383
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-06 02:16:29 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,13 @@
 class Hash
   def keys_of(*arguments)
 array = []
-  arguments.each do |key, value|
-    if value == arguments.values
+
+  arguments.each do |argument|
+    argument.each do |key, value|
+      if argument == value
       array << key
+        end
+      end
     end
   end
-  array
-end
 end
